e7708027f1c14744b5a1d2e503a13f4ae761ee57
Yoann Pradat <pradaty@Yoanns-MacBook-Pro.local>
2019-02-14 18:37:06 +0100

    8 models

--- apprentissage_profond/mauna_kea/source/models.py
+++ apprentissage_profond/mauna_kea/source/models.py
@@ -17,21 +17,21 @@ class BenchMark(nn.Module):
         self.n_classes = n_classes
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=2, padding=1)      # (3, 228, 228)
         self.pool1 = nn.MaxPool2d(kernel_size=2)                                                       # (3, 114, 114)
-        self.drop1 = nn.Dropout2d(p=0.5)
+        self.drop1 = nn.Dropout2d(p=0.2)
         self.conv2 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=2, padding=1)     # (16, 57, 57)
         self.pool2 = nn.MaxPool2d(kernel_size=2)                                                       # (16, 28, 28)
-        self.drop2 = nn.Dropout2d(p=0.5)
+        self.drop2 = nn.Dropout2d(p=0.2)
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=2, padding=1)    # (32, 14, 14)
         self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (32, 7, 7)
-        self.drop3 = nn.Dropout2d(p=0.25)
+        self.drop3 = nn.Dropout2d(p=0.2)
         
         self.fc1 = nn.Linear(1568, n_classes)
 
     def forward(self, x):
         # input is (bs, 1, 456, 456)
-        x = self.drop1(self.pool1(F.relu(self.conv1(x))))
-        x = self.drop2(self.pool2(F.relu(self.conv2(x))))
-        x = self.drop3(self.pool3(F.relu(self.conv3(x))))
+        x = self.pool1(self.drop1(F.relu(self.conv1(x))))
+        x = self.pool2(self.drop2(F.relu(self.conv2(x))))
+        x = self.pool3(self.drop3(F.relu(self.conv3(x))))
     
         x = x.view(x.size(0), -1)
         x = F.tanh(self.fc1(x))
@@ -41,52 +41,52 @@ class BenchMarkAug(nn.Module):
     def __init__(self, n_classes):
         super(BenchMark, self).__init__()
         self.n_classes = n_classes
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=2, padding=1)      # (3, 228, 228)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)                                                       # (3, 114, 114)
-        self.drop1 = nn.Dropout2d(p=0.5)
-        self.conv2 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=2, padding=1)     # (16, 57, 57)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)                                                       # (16, 28, 28)
-        self.drop2 = nn.Dropout2d(p=0.5)
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=2, padding=1)    # (32, 14, 14)
-        self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (32, 7, 7)
-        self.drop3 = nn.Dropout2d(p=0.25)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=2, padding=1)      # (3, 112, 112)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)                                                       # (3, 56, 56)
+        self.drop1 = nn.Dropout2d(p=0.2)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=2, padding=1)     # (16, 28, 28)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)                                                       # (16, 14, 14)
+        self.drop2 = nn.Dropout2d(p=0.2)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=2, padding=1)    # (64, 7, 7)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (64, 3, 3)
+        self.drop3 = nn.Dropout2d(p=0.2)
         
-        self.fc1 = nn.Linear(1568, n_classes)
+        self.fc1 = nn.Linear(384, n_classes)
 
     def forward(self, x):
-        # input is (bs, 1, 456, 456)
-        x = self.drop1(self.pool1(F.relu(self.conv1(x))))
-        x = self.drop2(self.pool2(F.relu(self.conv2(x))))
-        x = self.drop3(self.pool3(F.relu(self.conv3(x))))
+        # input is (bs, 1, 224, 224)
+        x = self.pool1(self.drop1(F.relu(self.conv1(x))))
+        x = self.pool2(self.drop2(F.relu(self.conv2(x))))
+        x = self.pool3(self.drop3(F.relu(self.conv3(x))))
     
         x = x.view(x.size(0), -1)
         x = F.tanh(self.fc1(x))
         return x
 
-class BenchMarkBn(nn.Module):
+class BenchMarkBnAug(nn.Module):
     def __init__(self, n_classes):
-        super(BenchMarkBn, self).__init__()
+        super(BenchMark, self).__init__()
         self.n_classes = n_classes
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=2, padding=1)      # (3, 228, 228)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=2, padding=1)      # (3, 112, 112)
         self.bn1 = nn.BatchNorm2d(3)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)                                                       # (3, 114, 114)
-        self.drop1 = nn.Dropout2d(p=0.5)
-        self.conv2 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=2, padding=1)     # (16, 57, 57)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)                                                       # (3, 56, 56)
+        self.drop1 = nn.Dropout2d(p=0.2)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=2, padding=1)     # (16, 28, 28)
         self.bn2 = nn.BatchNorm2d(16)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)                                                       # (16, 28, 28)
-        self.drop2 = nn.Dropout2d(p=0.5)
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=2, padding=1)    # (32, 14, 14)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (32, 7, 7)
-        self.drop3 = nn.Dropout2d(p=0.25)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)                                                       # (16, 14, 14)
+        self.drop2 = nn.Dropout2d(p=0.2)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=2, padding=1)    # (64, 7, 7)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (64, 3, 3)
+        self.drop3 = nn.Dropout2d(p=0.2)
         
-        self.fc1 = nn.Linear(1568, n_classes)
+        self.fc1 = nn.Linear(384, n_classes)
 
     def forward(self, x):
-        # input is (bs, 1, 456, 456)
-        x = self.drop1(self.pool1(F.relu(self.bn1(self.conv1(x)))))
-        x = self.drop2(self.pool2(F.relu(self.bn2(self.conv2(x)))))
-        x = self.drop3(self.pool3(F.relu(self.bn3(self.conv3(x)))))
+        # input is (bs, 1, 224, 224)
+        x = self.pool1(self.drop1(F.relu(self.bn1(self.conv1(x)))))
+        x = self.pool2(self.drop2(F.relu(self.bn2(self.conv2(x)))))
+        x = self.pool3(self.drop3(F.relu(self.bn3(self.conv3(x)))))
     
         x = x.view(x.size(0), -1)
         x = F.tanh(self.fc1(x))
@@ -105,12 +105,12 @@ class MaunaNet4(nn.Module):
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)   # (128, 15, 15)
         self.bn3 = nn.BatchNorm2d(128)
         self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (128, 8, 8)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=1024, kernel_size=3, stride=2, padding=1) # (1024, 4, 4)
-        self.bn4 = nn.BatchNorm2d(1024)
-        self.pool4 = nn.MaxPool2d(kernel_size=2)                                                       # (1024, 2, 2)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)  # (256, 4, 4)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.pool4 = nn.MaxPool2d(kernel_size=2)                                                       # (256, 2, 2)
         
-        self.avgpool = nn.AvgPool2d(2)
-        self.fc1 = nn.Linear(1024, n_classes)
+        self.avgpool = nn.AvgPool2d(2)                                                                 # (256, 1, 1)
+        self.fc1 = nn.Linear(256, n_classes)
 
     def forward(self, x):
         # input is (bs, 1, 456, 456)
@@ -118,6 +118,42 @@ class MaunaNet4(nn.Module):
         x = self.pool2(F.relu(self.bn2(self.conv2(x))))
         x = self.pool3(F.relu(self.bn3(self.conv3(x))))
         x = self.pool4(F.relu(self.bn4(self.conv4(x))))
+    
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = F.tanh(self.fc1(x))
+        return x
+
+class MaunaNet4Drop(nn.Module):
+    def __init__(self, n_classes):
+        super(MaunaNet4, self).__init__()
+        self.n_classes = n_classes
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=2, padding=1)      # (3, 228, 228)
+        self.bn1 = nn.BatchNorm2d(3)
+        self.drop1 = nn.Dropout2d(p=0.2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)                                                       # (3, 114, 114)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=2, padding=1)     # (64, 57, 57)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.drop2 = nn.Dropout2d(p=0.2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)                                                       # (64, 29, 29)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)   # (128, 15, 15)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.drop3 = nn.Dropout2d(p=0.2)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (128, 8, 8)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)  # (256, 4, 4)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.drop4 = nn.Dropout2d(p=0.2)
+        self.pool4 = nn.MaxPool2d(kernel_size=2)                                                       # (256, 2, 2)
+        
+        self.avgpool = nn.AvgPool2d(2)                                                                 # (256, 2, 2)
+        self.fc1 = nn.Linear(256, n_classes)
+
+    def forward(self, x):
+        # input is (bs, 1, 456, 456)
+        x = self.pool1(self.drop1(F.relu(self.bn1(self.conv1(x)))))
+        x = self.pool2(self.drop2(F.relu(self.bn2(self.conv2(x)))))
+        x = self.pool3(self.drop3(F.relu(self.bn3(self.conv3(x)))))
+        x = self.pool4(self.drop4(F.relu(self.bn4(self.conv4(x)))))
     
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
@@ -150,6 +186,42 @@ class MaunaNet4Aug(nn.Module):
         x = self.pool2(F.relu(self.bn2(self.conv2(x))))
         x = self.pool3(F.relu(self.bn3(self.conv3(x))))
         x = self.pool4(F.relu(self.bn4(self.conv4(x))))
+    
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = F.tanh(self.fc1(x))
+        return x
+
+class MaunaNet4AugDrop(nn.Module):
+    def __init__(self, n_classes):
+        super(MaunaNet4Aug, self).__init__()
+        self.n_classes = n_classes
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=2, padding=1)      # (3, 112, 112)
+        self.bn1 = nn.BatchNorm2d(3)
+        self.drop1 = nn.Dropout2d(p=0.2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)                                                       # (3, 56, 56)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)     # (64, 56, 56)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.drop2 = nn.Dropout2d(p=0.2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)                                                       # (64, 28, 28)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)   # (128, 28, 28)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.drop3 = nn.Dropout2d(p=0.2)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)                                                       # (128, 14, 14)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)  # (256, 14, 14)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.drop4 = nn.Dropout2d(p=0.2)
+        self.pool4 = nn.MaxPool2d(kernel_size=2)                                                       # (256, 7, 7)
+        
+        self.avgpool = nn.AvgPool2d(7)                                                                 # (256, 1, 1)
+        self.fc1 = nn.Linear(256, n_classes)
+
+    def forward(self, x):
+        # input is (bs, 1, 224, 224)
+        x = self.pool1(self.drop1(F.relu(self.bn1(self.conv1(x)))))
+        x = self.pool2(self.drop2(F.relu(self.bn2(self.conv2(x)))))
+        x = self.pool3(self.drop3(F.relu(self.bn3(self.conv3(x)))))
+        x = self.pool4(self.drop4(F.relu(self.bn4(self.conv4(x)))))
     
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
